a102ffbc8c0850b1f8664939e194aaddfe745f6d
marzia <you@example.com>
2025-07-01 15:36:38 +0200

    confirm crud operations works

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3030;
 
+const postsRouter = require("./routes/posts.js");
+
+app.use(express.static("imgs"));
+
+/*
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello marzia");
 });
+*/
+
+app.use("/posts", postsRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost${port}`);
