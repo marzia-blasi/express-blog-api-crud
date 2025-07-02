935a6c7353f927c0fba589648a9c3f27edb44d3b
marzia <you@example.com>
2025-07-02 12:54:36 +0200

    Implement CRUD operations for posts in the postController

---
+++ controllers/postController.js
@@ -0,0 +1,115 @@
+//importiamo posts da data
+
+const posts = require("../data/posts.js");
+
+// Index + bonus
+(req, res) => {
+  // res.json(posts);
+  let filterposts = posts;
+
+  if (req.query.tags) {
+    filterposts = posts.filter((post) => post.tags.includes(req.query.tags));
+  }
+
+  res.json(filterposts);
+};
+
+// Show + Bonus
+(req, res) => {
+  console.log(req.params);
+  //conversione id in numero
+  const id = parseInt(req.params.id);
+
+  //ricerca dell'id (cicla dentro posts per restituirci il singolo elemento post)
+  const post = posts.find((posts) => posts.id === id);
+
+  if (!post) {
+    return res.json({
+      error: "Not Found",
+      message: "Post non trovato",
+    });
+  }
+
+  res.json(post);
+};
+
+//Store
+(req, res) => {
+  console.log(req.body);
+
+  //nuovo id che s'incrementa dopo quello già esistente
+  const newId = posts[posts.length - 1].id + 1;
+  //oggetto post
+  const newPost = {
+    id: newId,
+    title: req.body.title,
+    content: req.body.content,
+    image: req.body.image,
+    tags: req.body.tags,
+  };
+
+  //aggiunta a posts
+  posts.push(newPost);
+
+  console.log(posts);
+
+  // status + json
+  res.status(201);
+  res.json(newPost);
+};
+
+//Update
+(req, res) => {
+  //conversione id in numero
+  const id = parseInt(req.params.id);
+
+  //ricerca dell'id
+  const post = posts.find((posts) => posts.id === id);
+
+  if (!post) {
+    return res.json({
+      error: "Not Found",
+      message: "Post non trovato",
+    });
+  }
+
+  // parametro da aggiornare
+  (post.title = req.body.title),
+    (post.content = req.body.content),
+    (post.image = req.body.image),
+    (post.tags = req.body.tags),
+    console.log(posts);
+
+  res.json(post);
+};
+
+//Modify
+(req, res) => {
+  const id = req.params.id;
+  res.send(`mododifica parzialmente il post n ${id}`);
+};
+
+//Destroy + Bonus
+(req, res) => {
+  //conversione id in numero
+  const id = parseInt(req.params.id);
+
+  //ricerca dell'id
+  const post = posts.find((posts) => posts.id === id);
+
+  if (!post) {
+    res.status(404);
+
+    return res.json({
+      status: 404,
+      error: "Not Found",
+      message: "Post non trovato",
+    });
+  }
+  // troviamo il post e lo rimuoviamo
+  posts.splice(posts.indexOf(post), 1);
+  console.log(posts);
+
+  //status
+  res.sendStatus(204);
+};
--- routes/posts.js
+++ routes/posts.js
@@ -3,138 +3,24 @@ const router = express.Router();
 
 //importiamo posts da data
 
-const posts = require("../data/posts.js");
+//const posts = require("../data/posts.js");
 
 // Index + bonus
-router.get("/", (req, res) => {
-  // res.json(posts);
-  let filterposts = posts;
-
-  if (req.query.tags) {
-    filterposts = posts.filter((post) => post.tags.includes(req.query.tags));
-  }
-
-  res.json(filterposts);
-});
+router.get("/");
 
 // Show + Bonus
-router.get("/:id", (req, res) => {
-  console.log(req.params);
-
-  //const id = req.params.id;
-
-  //conversione id in numero
-  const id = parseInt(req.params.id);
-
-  //ricerca dell'id (cicla dentro posts per restituirci il singolo elemento post)
-  const post = posts.find((posts) => posts.id === id);
-
-  //res.send(`post numero ${id}`);
-
-  //se il post richiesto non esiste "post non trovato"
-
-  if (!post) {
-    return res.json({
-      error: "Not Found",
-      message: "Post non trovato",
-    });
-  }
-
-  res.json(post);
-});
+router.get("/:id");
 
 //Store
-router.post("/", (req, res) => {
-  // res.send(`crea nuovo post`);
-  console.log(req.body);
-
-  //nuovo id che s'incrementa dopo quello già esistente
-  const newId = posts[posts.length - 1].id + 1;
-  //oggetto post
-  const newPost = {
-    id: newId,
-    title: req.body.title,
-    content: req.body.content,
-    image: req.body.image,
-    tags: req.body.tags,
-  };
-
-  //aggiunta a posts
-  posts.push(newPost);
-
-  console.log(posts);
-
-  // status + json
-  res.status(201);
-  res.json(newPost);
-});
+router.post("/");
 
 //Update
-router.put("/:id", (req, res) => {
-  /*
-  const id = req.params.id;
-  res.send(`modifica tutto il post n ${id}`);
-  */
-
-  //conversione id in numero
-  const id = parseInt(req.params.id);
-
-  //ricerca dell'id (cicla dentro posts per restituirci il singolo elemento post)
-  const post = posts.find((posts) => posts.id === id);
-
-  if (!post) {
-    return res.json({
-      error: "Not Found",
-      message: "Post non trovato",
-    });
-  }
-
-  // parametro da aggiornare
-  (post.title = req.body.title),
-    (post.content = req.body.content),
-    (post.image = req.body.image),
-    (post.tags = req.body.tags),
-    console.log(posts);
-
-  res.json(post);
-});
+router.put("/:id");
 
 //Modify
-router.patch("/:id", (req, res) => {
-  const id = req.params.id;
-  res.send(`mododifica parzialmente il post n ${id}`);
-});
+router.patch("/:id");
 
 //Destroy + Bonus
-router.delete("/:id", (req, res) => {
-  /*
-  const id = req.params.id;
-    res.send(`cancella tutto il post ${id}`);
-    */
-
-  //conversione id in numero
-  const id = parseInt(req.params.id);
-
-  //ricerca dell'id (cicla dentro posts per restituirci il singolo elemento post)
-  const post = posts.find((posts) => posts.id === id);
-
-  //se il post richiesto non esiste "post non trovato"
-
-  if (!post) {
-    res.status(404);
-
-    return res.json({
-      status: 404,
-      error: "Not Found",
-      message: "Post non trovato",
-    });
-  }
-  // troviamo il post e lo rimuoviamo
-  posts.splice(posts.indexOf(post), 1);
-  console.log(posts);
-
-  //status
-  res.sendStatus(204);
-});
+router.delete("/:id");
 
 module.exports = router;
